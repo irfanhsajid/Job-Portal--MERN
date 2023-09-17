f0999074b8d38a6cfcce3d16e38a5a28ba25911b
Irfanul Haque Sajid <irfanhsajid@gmail.com>
2023-09-17 08:50:51 +0600

    Updating authRoutes.js by cors origin " "

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -14,7 +14,7 @@ const { test, registerUser, loginUser, getProfile, logoutUser, getUsers } = requ
 router.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:5173'
+        origin: " ",
     })
 )
 
