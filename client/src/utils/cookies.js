886f46d021ee6ae13e3c086d5f82392fc836fc4a
Irfanul Haque Sajid <irfanhsajid@gmail.com>
2023-09-20 11:34:31 +0600

    Updated cookies.js

--- client/src/utils/cookies.js
+++ client/src/utils/cookies.js
@@ -1,9 +1,9 @@
-const oneYearInSeconds = 365 * 24 * 60 * 60; // Calculate the number of seconds in a year
-setCookie("myCookie", "myValue", oneYearInSeconds);
+//const oneYearInSeconds = 365 * 24 * 60 * 60; // Calculate the number of seconds in a year
+//setCookie("myCookie", "myValue", oneYearInSeconds);
 
 export function setCookie(cname, cvalue, exdays) {
     const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    d.setTime(d.getTime() + (exdays * 365 * 24 * 60 * 60 * 1000)); //updated for 1 year
     let expires = "expires=" + d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
