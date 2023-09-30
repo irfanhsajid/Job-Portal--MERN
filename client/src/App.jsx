880a8ab6e4b124ccccce975bd16544ee9775eedf
Irfanul Haque Sajid <irfanhsajid@gmail.com>
2023-09-30 14:00:56 +0600

    frontend hosted to netlify and cors updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,12 +12,10 @@ import Register from './pages/Register'
 import PrivateRoutes from './utils/PrivateRoutes'
 
 //cors policy setup
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://auth-skeleton-api.vercel.app';
 axios.defaults.withCredentials = true;
 
 function App() {
-
-
   return (
     <>
       <UserContextProvider>
